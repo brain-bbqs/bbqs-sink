a3add00c828a607c507f7ec1349ae34eb6618448
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-03-09 17:58:15 -0400

    add basic example (#1)

---
+++ decode_ttl_sequence/README.md
@@ -0,0 +1,20 @@
+# Decode TTL Sequence
+
+TTL pulses can be sent as simple on/off trigger times, but they can also be sent as a sequence of pulses that encode information in the timing or amplitude of the pulses.
+For example, a sequence of 3 pulses with varying voltage levels could encode 3 bits of information (`000`, `001`, `010`, `011`, `100`, `101`, `110`, `111`).
+
+This utility provides a way that has been used to decode voltage-varying TTL sequences into integer values.
+This specific method was used by the [Feldman lab](https://www.feldmanlab.org/) at UC Berkeley to encode the trial number during an experiment using Neuropixels probes with a NIDQ board.
+The TTL pulses were scaled into an intermediate hex-code (16-bit) representation, which is mapped to the integer at the final step.
+
+This code has been adapted from a previous [data conversion project](https://github.com/catalystneuro/feldman-lab-to-nwb/blob/6938040c2846ff6ca4f5a4530e9102960927e006/feldman_lab_to_nwb/utils.py#L1C1-L366C61).
+
+
+
+### Installation
+
+Using any Python setup, install the minimal dependencies with:
+
+```bash
+pip install numpy
+```
---
+++ decode_ttl_sequence/code/decode_ttl_sequence.py
@@ -0,0 +1,106 @@
+import numpy
+from tqdm import tqdm
+
+
+def get_trials_info(
+    trial_trace: numpy.ndarray,
+    event_trace: numpy.ndarray,
+    sampling_rate: float,
+    voltage_range: float,
+) -> tuple[numpy.ndarray, numpy.ndarray]:
+    """
+    A basic utility function to decode the trial information from the TTL channels in the SpikeGLX recordings.
+
+    The trial information is encoded in a sequence of 4 hexadecimal digits, where each digit is represented by a
+    voltage level on the 'event signal' channel.
+
+    The 'trial onset signal' channel is used to identify the start and end of each trial.
+
+    Parameters
+    ----------
+    trial_trace : numpy array
+        The extracted voltage trace corresponding to the 'trial onset signal' (TTL channel) from the recording.
+    event_trace : numpy array
+        The extracted voltage trace corresponding to the 'event signal' (TTL channel) from the recording.
+    sampling_rate : float
+        The sampling rate of the recording, used to convert sample indices to time.
+    voltage_range : float
+        The voltage range of the TTL signals, used to scale the event trace to the appropriate range for decoding.
+        Example values used for NIDQ boards might be around `4.5 * 1e6`.
+
+    Returns
+    -------
+    trial_numbers: numpy array
+        Array with decoded trial ID for each trial.
+    trial_times: numpy array
+        Array with `t_start` and `t_stop` timestamps for each trial.
+    """
+    hex_base = 16
+    hex_dict = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "a",
+        11: "b",
+        12: "c",
+        13: "d",
+        14: "e",
+        15: "f",
+    }
+
+    tenms_interval = int(0.01 * sampling_rate)
+
+    scaled_tr_events = event_trace * (hex_base - 1) / voltage_range
+    scaled_tr_events = (
+        (scaled_tr_events - numpy.median(scaled_tr_events)) / numpy.max(scaled_tr_events) * (hex_base - 1)
+    )
+
+    tr_trial_bin = numpy.zeros(trial_trace.shape, dtype=int)
+    tr_trial_bin[trial_trace > numpy.max(trial_trace) // 2] = 1
+
+    t_start_idxs = numpy.where(numpy.diff(tr_trial_bin) > 0)[0]
+    t_stop_idxs = numpy.where(numpy.diff(tr_trial_bin) < 0)[0]
+
+    # discard first stop event if it comes before a start event
+    if t_stop_idxs[0] < t_start_idxs[0]:
+        print("Discarding first trial")
+        t_stop_idxs = t_stop_idxs[1:]
+
+    # discard last start event if it comes after last stop event
+    if t_start_idxs[-1] > t_stop_idxs[-1]:
+        print("Discarding last trial")
+        t_start_idxs = t_start_idxs[:-1]
+
+    if len(t_start_idxs) == 0 or len(t_stop_idxs) == 0:
+        message = "No trial start or stop events found in the provided traces!"
+        raise ValueError(message)
+
+    trial_numbers = []
+    trial_times = []
+
+    for t in tqdm(iterable=range(len(t_start_idxs)), desc="Parsing hex signals"):
+        start_idx = t_start_idxs[t]
+        stop_idx = t_stop_idxs[t]
+
+        trial_times.append(numpy.array([start_idx, stop_idx]) / sampling_rate)
+
+        i_start = start_idx
+        trial_digits = ""
+
+        for i in range(4):
+            digit = numpy.median(scaled_tr_events[i_start + 10 : i_start + tenms_interval - 10])
+            digit = int(round(digit))
+            trial_digits += hex_dict[digit]
+            i_start += tenms_interval
+        trial_numbers.append(int(trial_digits, hex_base))
+
+    trial_number_array = numpy.array(trial_numbers)
+    trial_times_array = numpy.array(trial_times)
+    return trial_number_array, trial_times_array
